8b7747d219d248e93647dbdd25219c5b3a8cc2cd
ume09 <umetaka0929@gmail.com>
2025-01-04 23:56:34 +0900

    Update test.bash

--- test/test.bash
+++ test/test.bash
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# デフォルトディレクトリはホームディレクトリ
 DIR=~
-[ "$1" != "" ] && DIR="$1"  # 引数があったら、それをディレクトリとして使用
+[ "$1" != "" ] && DIR="$1" 
 
-# ROS 2ワークスペースに移動してビルド
 cd $DIR/ros2_ws
 colcon build
 source $DIR/.bashrc
 
-# プログラムを実行して出力をログに保存
 timeout 10 python3 ros2_time_publisher.py > /tmp/ros2_time_publisher.log
 
 # ログに特定の文字列が含まれているか確認
