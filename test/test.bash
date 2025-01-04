77e8ab58a6ef4375c594fb755f41e1b199da3491
ume09 <umetaka0929@gmail.com>
2025-01-04 14:42:05 +0900

    Update test.bash

--- test/test.bash
+++ test/test.bash
@@ -1,12 +1,17 @@
 #!/bin/bash
-　
-dir=~
-[ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
-　　     　　　
-cd $dir/ros2_ws
-colcon build
-source $dir/.bashrc 　
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log |
-grep 'Listen: 10'
+# デフォルトディレクトリはホームディレクトリ
+DIR=~
+[ "$1" != "" ] && DIR="$1"  # 引数があったら、それをディレクトリとして使用
+
+# ROS 2ワークスペースに移動してビルド
+cd $DIR/ros2_ws
+colcon build
+source $DIR/.bashrc
+
+# プログラムを実行して出力をログに保存
+timeout 10 python3 ros2_time_publisher.py > /tmp/ros2_time_publisher.log
+
+# ログに特定の文字列が含まれているか確認
+cat /tmp/ros2_time_publisher.log |
+grep -E '現在時刻: .*?, 残り時間: [0-9]+分'
