b8a99b91273b1ad229c88496800f665c2f2304f5
Hidetaka Umehara <umetaka0929@gmail.com>
2025-01-04 13:14:04 +0900

    Add four filles for the first commit

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'mypkg'
@@ -10,10 +12,11 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Hidetaka Umehara',
+    maintainer='ume09',
     maintainer_email='umetaka0929@gmail.com',
     description='a package for practice',
     license='BSD-3-Clause',
@@ -21,7 +24,7 @@ setup(
     entry_points={
         'console_scripts': [
             'talker = mypkg.talker:main',
-            #'listener = mypkg.listener:main',
+            'listener = mypkg.listener:main',
         ],
     },
-)
+    )
