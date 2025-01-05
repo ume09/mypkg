0279304f8252f268e0f9084700184e40f20626f5
ume09 <umetaka0929@gmail.com>
2025-01-06 02:39:44 +0900

    Update test.bash

--- test/test.bash
+++ test/test.bash
@@ -10,9 +10,10 @@ colcon build
 source $DIR/.bashrc
 
 # プログラムを実行して出力をログに保存
-timeout 10 python3 ros2_time_publisher.py > /tmp/ros2_time_publisher.log
+#timeout 10 python3 ros2_time_publisher.py > /tmp/ros2_time_publisher.log
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 # ログに特定の文字列が含まれているか確認
 cat /tmp/ros2_time_publisher.log |
 grep -E '現在時刻: .*?, 残り時間: [0-9]+分'
-grep -E '現在時刻: .*?, 残り時間: [0-9]+分' && echo "テスト成功" || echo "テスト失敗"
+grep -E '現在時刻: .*?, 残り時間: [0-9]+分'
