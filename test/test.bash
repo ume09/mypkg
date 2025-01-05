5693762452b7e89d0041bfcea5d248a6d42f4751
ume09 <umetaka0929@gmail.com>
2025-01-05 11:12:41 +0900

    Update test.bash

--- test/test.bash
+++ test/test.bash
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-
+# デフォルトディレクトリはホームディレクトリ
 DIR=~
-[ "$1" != "" ] && DIR="$1" 
+[ "$1" != "" ] && DIR="$1"  # 引数があったら、それをディレクトリとして使用
 
-cd $DIR/ros2_ws || { echo "ディレクトリ $DIR/ros2_ws が存在しません"; exit 1; }
-colcon build || { echo "ビルドに失敗しました"; exit 1; }
-source install/setup.bash
+# ROS 2ワークスペースに移動してビルド
+cd $DIR/ros2_ws
+colcon build
+source $DIR/.bashrc
 
-timeout 10 python3 src/ros2_time_publisher.py > /tmp/ros2_time_publisher.log || { echo "プログラムの実行に失敗しました"; exit 1; }
+# プログラムを実行して出力をログに保存
+timeout 10 python3 ros2_time_publisher.py > /tmp/ros2_time_publisher.log
 
-
-if grep -E '現在時刻: .*?, 残り時間: [0-9]+分' /tmp/ros2_time_publisher.log; then
-    echo "テスト成功"
-else
-    echo "テスト失敗"
-    exit 1
-fi
+# ログに特定の文字列が含まれているか確認
+cat /tmp/ros2_time_publisher.log |
+grep -E '現在時刻: .*?, 残り時間: [0-9]+分'
+grep -E '現在時刻: .*?, 残り時間: [0-9]+分' && echo "テスト成功" || echo "テスト失敗"
