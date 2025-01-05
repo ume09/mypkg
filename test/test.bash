5f80686a74c56377d4fe5e0abb1f78be0b47bc4c
ume09 <umetaka0929@gmail.com>
2025-01-06 02:52:57 +0900

    Update test.bash

--- test/test.bash
+++ test/test.bash
@@ -1,16 +1,12 @@
 #!/bin/bash
 
 DIR=~
-[ "$1" != "" ] && DIR="$1"  # 引数があったら、それをディレクトリとして使用
+[ "$1" != "" ] && DIR="$1"
 
-# ROS 2ワークスペースに移動してビルド
 cd $DIR/ros2_ws
 colcon build
 source $DIR/.bashrc
 
-# プログラムを実行して出力をログに保存
-timeout 10 python3 ros2_time_publisher.py > /tmp/ros2_time_publisher.log
+timeout 10 python3 study_timer.py > /tmp/study_timer.log
 
-# ログに特定の文字列が含まれているか確認
-cat /tmp/ros2_time_publisher.log |
-grep -E '現在時刻: .*?, 残り時間: [0-9]+分' && echo "テスト成功" || echo "テスト失敗"
+grep -E '現在時刻: .*?, 残り時間: [0-9]+分' /tmp/study_timer.log && echo "テスト成功" || echo "テスト失敗"
