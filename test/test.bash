a4072d304e84fe9704bf0ce73c975123d2681f89
ume09 <umetaka0929@gmail.com>
2025-01-06 02:43:43 +0900

    Update test.bash

--- test/test.bash
+++ test/test.bash
@@ -10,8 +10,7 @@ colcon build
 source $DIR/.bashrc
 
 # プログラムを実行して出力をログに保存
-# timeout 10 python3 ros2_time_publisher.py > /tmp/ros2_time_publisher.log
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 10 python3 ros2_time_publisher.py > /tmp/ros2_time_publisher.log
 
 # ログに特定の文字列が含まれているか確認
 cat /tmp/ros2_time_publisher.log |
