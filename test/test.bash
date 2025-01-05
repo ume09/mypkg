0790f4e2c8326f57a1a4fd0d69b88df83eac05aa
ume09 <umetaka0929@gmail.com>
2025-01-06 02:55:31 +0900

    Update test.bash

--- test/test.bash
+++ test/test.bash
@@ -7,6 +7,6 @@ cd $DIR/ros2_ws
 colcon build
 source $DIR/.bashrc
 
-timeout 10 python3 study_timer.py > /tmp/study_timer.log
+timeout 10 python3　ros2 study_timer.py > /tmp/study_timer.log
 
 grep -E '現在時刻: .*?, 残り時間: [0-9]+分' /tmp/study_timer.log && echo "テスト成功" || echo "テスト失敗"
