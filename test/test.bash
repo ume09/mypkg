fc559b7b90a3c008e34057fd769fea8740f4623c
ume09 <umetaka0929@gmail.com>
2025-01-06 20:14:25 +0900

    Update test.bash

--- test/test.bash
+++ test/test.bash
@@ -10,8 +10,10 @@ colcon build
 source $dir/.bashrc
 ource /opt/ros/humble/setup.bash
 
-timeout 10 ros2 launch mypkg study_timer.launch.py > /tmp/mypkg.log
+# timeout 10 ros2 launch mypkg study_timer.launch.py > /tmp/mypkg.log
 # timeout 10 ros2 run mypkg study_timer > /tmp/mypkg.log
+timeout 10 python3 study_timer.py > /dev/null 2>&1 &
+timeout 10 ros2 topic echo /time_info > /tmp/study_timer.log &
 # timeout 10 ros2 topic echo /time_info > /tmp/mypkg.log
 
 sleep 10
