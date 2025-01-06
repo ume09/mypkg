0dad3960751c5e494099e2f865d21e1cca8a7bbc
ume09 <umetaka0929@gmail.com>
2025-01-06 21:01:59 +0900

    Update listener.py

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,1 +1,18 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int16
 
+
+rclpy.init()
+node = Node("listener")
+
+
+def cb(msg):
+    global node
+    node.get_logger().info("Listen: %d" % msg.data)
+
+
+def main():
+  pub = node.create_subscription(Int16, "time_info", cb, 10)
+  rclpy.spin(node)
+_info
