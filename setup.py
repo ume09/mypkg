f28bc9aeab944ab3dfbc0ec5e8340f11d7197208
Hidetaka Umehara <umetaka0929@gmail.com>
2025-01-06 22:24:03 +0900

    Add four filles for the first commit

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
     entry_points={
         'console_scripts': [
             'study_timer = mypkg.study_timer:main',
+            'listener = mypkg.listener:main',
          
         ],
     },
