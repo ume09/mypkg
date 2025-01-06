363da361ff26c48bf6eb0e31002f71cbe071ed98
ume09 <umetaka0929@gmail.com>
2025-01-06 20:25:19 +0900

    Update test.bash

--- test/test.bash
+++ test/test.bash
@@ -10,6 +10,9 @@ colcon build
 source $dir/.bashrc
 ource /opt/ros/humble/setup.bash
 
+LOG_FILE="/tmp/mypkg.log"
+> $LOG_FILE
+
 # timeout 10 ros2 launch mypkg study_timer.launch.py > /tmp/mypkg.log
 # timeout 10 ros2 run mypkg study_timer > /tmp/mypkg.log
 timeout 10 python3 study_timer.py > /dev/null 2>&1 &
@@ -17,9 +20,11 @@ timeout 10 ros2 topic echo /time_info > /tmp/mypkg.log &
 # timeout 10 ros2 topic echo /time_info > /tmp/mypkg.log
 
 sleep 10
-
+LOG_FILE="/tmp/mypkg.log"
 if grep -q "現在時刻" /tmp/mypkg.log; then
   echo "成功"
 else
   echo "エラー"
+  cat $LOG_FILE
+fi
 fi
