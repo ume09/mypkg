ac0e2a586d403cacfa6ea32df51e87d7e3821800
ume09 <umetaka0929@gmail.com>
2025-01-06 15:39:15 +0900

    Update test.bash

--- test/test.bash
+++ test/test.bash
@@ -2,8 +2,8 @@
 # SPDX-FileCopyrightText: 2025 Hidetaka Umehara
 # SPDX-License-Identifier: BSD-3-Clause
 
-DIR=~
-[ "$1" != "" ] && DIR="$1"
+dir=~
+[ "$1" != "" ] && dir="$1"
 
 cd $dir/ros2_ws
 colcon build
@@ -12,4 +12,4 @@ source $dir/.bashrc
 timeout 10 ros2 launch mypkg study_timer.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
-grep -E '現在時刻: [0-9-]+ [0-9:]+, 残り時間: [0-9]+分' /tmp/study_timer.log && echo "テスト成功" || echo "テスト失敗"
+grep -E '現在時刻: [0-9-]+ [0-9:]+, 残り時間: [0-9]+分'
